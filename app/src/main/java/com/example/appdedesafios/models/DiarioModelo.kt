da5771cc5ec6b960a3bd16ec595ee6f3b04abdc7
EnzoGabss <brendabettini07@gmail.comgit config --global user.email>
2024-05-23 21:14:31 -0300

    Initial commit

---
+++ app/src/main/java/com/example/appdedesafios/models/DiarioModelo.kt
@@ -0,0 +1,7 @@
+package com.example.appdedesafios.models
+
+data class DiarioModelo (
+    var diarioId: String? = null,
+    var feedback: String? = null,
+){
+}
---
+++ app/src/main/java/com/example/appdedesafios/models/InformaModelo.kt
@@ -0,0 +1,11 @@
+package com.example.appdedesafios.models
+
+data class InformaModelo(
+    var UsuaId: String? = null,
+    var doce: String? = null,
+    var salgado: String? = null,
+    var musica: String? = null,
+    var filme: String? = null
+) {
+
+}
